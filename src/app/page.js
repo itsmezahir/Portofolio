c887878e357fe6f86534def55485cff6be37f681
Taufiq <taufiqmaulanapr@gmail.com>
2025-10-19 11:05:42 +0700

    Zaahir2

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <>
-      <div>Welcome to the Home Page</div>
+      <div className="text-green-600">Welcome to the Home Page</div>
     </>
   )
 }
